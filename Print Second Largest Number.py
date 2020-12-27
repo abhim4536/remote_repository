037854dd61e459bb3debc295e8344ce9d28c27c2
Abhishek Maurya <007akk@gmail.com>
2020-12-27 20:26:56 +0530

    diary pro

--- Print Second Largest Number.py
+++
@@ -1,8 +0,0 @@
-#Print Second Largest Number
-a=[]
-n=int(input("Enter The Number Of Elements:"))
-for i in range(1,n+1):
-    b=int(input("Enter The Element:"))
-    a.append(b)
-a.sort()
-print("Print Second Largest Number Is:",a[-2])    
--- Word Filter.py
+++
@@ -1,6 +0,0 @@
-from better_profanity import profanity
-profanity.load_censor_words()
-
-text=input("Enter Your Word To Censor:")
-correct=profanity.censor(text,"*")
-print(correct)
